7fb940651ad6a4684ff2654f399e005760c409bc
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2022-09-25 21:43:38 +0500

    just checkin

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait  #..............
 from selenium.webdriver.support import expected_conditions as EC    #................
-from bs4 import BeautifulSoup
-import xlsxwriter
-import time
+# from bs4 import BeautifulSoup
+# import xlsxwriter
+# import time
 # from selenium.webdriver.common.proxy import Proxy, ProxyType  #.................
 # from selenium.webdriver.chrome.options import Options
 # from selenium.webdriver.chrome.service import Service
@@ -12,9 +12,11 @@ import time
 from msvcrt import getche, getch
 import os
 
+# url = "https://www.google.com/search?as_st=y&tbm=isch&as_q=&as_epq=badshahi+mosque&as_oq=&as_eq=&cr=&as_sitesearch=&safe=images&tbs=isz:lt,islt:xga"
+
 def gen_driver(self):
     chrome_options = uc.ChromeOptions()
     chrome_options.headless = True
-    chrome_options.add_argument('--proxy-server=http://'+PROXY)
+    # chrome_options.add_argument('--proxy-server=http://'+PROXY)
     driver = uc.Chrome(options=chrome_options)
     return driver
